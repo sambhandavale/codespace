0384b7388abb2678c3d8589fcad1767d6081f253
Samarth Bhandavale <sambhandavale15@gmail.com>
2024-10-01 20:10:36 +0530

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:4321,
+  server: {
+    port: 4321,
   },
   build: {
-    outDir: 'build',  // Set the output directory to 'build'
+    outDir: 'build',  // Ensure this is set to 'build'
   },
-})
+});
